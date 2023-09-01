8e975079fbc0f9620cfa4b72d35a3da32631d288
Mark Struck <markstruck@comcast.net>
2023-09-01 06:45:00 -0500

    added endpoint to call in to retrieve mlb stats

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
+const MLBStatsAPI = require('mlb-stats-api');
 
 const app = express();
 const port = 3000;
+
+const mlbStats = new MLBStatsAPI();
 
 const corsOptions = {
   origin: '*', // Allow requests from any origin
@@ -56,6 +59,13 @@ app.get('/baseball_scores', (req, res) => {
     error: null
   });
 })
+
+app.get('/test_mlb_stats', async (req, res) => {
+  const response = await mlbStats.getSchedule({ params: { sportId: 1 } });
+  const result = await response.json();
+  console.log(result);
+  res.send('successfully called test ap');
+});
 
 // Start the server
 app.listen(port, () => {
