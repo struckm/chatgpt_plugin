6d9b3a71d6ad1012d7b28d9cd18df9baf49eddd2
Mark Struck <markstruck@comcast.net>
2023-08-31 20:33:39 -0500

    added path to return back the openapi.yaml file

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = 3000;
 
 // Define a route for the root URL
 app.get('/', (req, res) => {
   res.send('Hello, World! This is your Express server.');
 });
+
+app.get('/openapi.yaml', (req, res) => {
+    const filePath = path.join(__dirname, 'openapi.yaml');
+
+    res.sendFile(filePath, (err) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Error sending the file.');
+        } else {
+            console.log('File sent: openapi.yaml');
+        }
+  });
+});
+  
 
 // Start the server
 app.listen(port, () => {
